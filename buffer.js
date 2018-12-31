7a0a39eb2bab0e78bf4e9ccb89bb8d8770e850f9
niesssiobhan <niesssiobhan@live.com>
2018-12-31 13:23:46 -0800

    worked on buffer.js

--- buffer.js
+++ buffer.js
@@ -1,23 +1,40 @@
 'use strict';
 
-// let array = ['Jared', 'Teagan', 'Siobhan'];
-// array.forEach( name => {
-//   console.log(name);
-// });
+let array = ['Jared', 'Teagan', 'Siobhan'];
+array.forEach( name => {
+  console.log(name);
+});
 
 const fs = require('fs');
 
-let bufferOne = new Buffer('\'use strict\';\n');
-let bufferTwo = new Buffer('let array = [\'Jared\', \'Teagan\', \'Siobhan\'];\n');
-let bufferThree = new Buffer('array.forEach( name => {\n');
-let bufferFour = new Buffer(' console.log(name)\n');
-let bufferFive = new Buffer('});');
+let filePath = './files/pair-programming.html';
 
-let array = [bufferOne, bufferTwo, bufferThree, bufferFour, bufferFive];
+// let bufferOne = new Buffer('\'use strict\';\n');
+// let bufferTwo = new Buffer('let array = [\'Jared\', \'Teagan\', \'Siobhan\'];\n');
+// let bufferThree = new Buffer('array.forEach( name => {\n');
+// let bufferFour = new Buffer(' console.log(name)\n');
+// let bufferFive = new Buffer('});');
 
-let finalBuffer = Buffer.concat(array);
+function createBuffer(str) {
+  let buffer = new Buffer.from('');
+  for(let i = 0; i < str.length; i++){
+    buffer += new Buffer.from(str[i]);
+  }
+  return buffer;
+}
 
-fs.writeFile('./files/loop.js', finalBuffer, err => {
-  if(err) { throw err; }
-  console.log('stuff');
+fs.readFile(path, function callback(err, data) {
+  if(err) {throw (err);}
+  console.log('reading file');
+  
+  fs.writeFile(filePath, data, (err) => {
+    if(err) {throw (err);}
+    console.log('the file has been successfully written');
+  });
 });
+
+// let array = [bufferOne, bufferTwo, bufferThree, bufferFour, bufferFive];
+
+// let finalBuffer = Buffer.concat(array);
+let buffer = createBuffer(array);
+createfile('./files/loop.js', buffer);
